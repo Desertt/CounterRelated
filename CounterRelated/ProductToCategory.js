7e4afa89562b8e042e4e544bb4fa081d57508054
Desertt <mustafadagtekin@gmail.com>
2020-12-16 02:09:51 +0300

    CounterRelated

---
+++ CounterRelated/ProductToCategory.js
@@ -0,0 +1,9 @@
+﻿var ctgryIdOne = "77";
+var ctgryIdTwo = "83";
+var ctgryIdThree = 0;
+var Icount = 2;
+
+for (var i = 2065; i <= 2152; i++) {
+   
+    document.write(i + " " + ctgryIdOne + "<br>" + i +" "+ ctgryIdTwo + "<br>");
+}
---
+++ CounterRelated/RelateNumbers.js
@@ -0,0 +1,8 @@
+﻿for (var i = 2065; i <= 2152; i++) {
+    for (var j = 2065; j <= 2152; j++) {
+        if (i!=j) {
+            document.write(i + " " + j + "<br>")
+        }
+    }
+    
+}
